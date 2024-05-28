b54597d0aae2483a301af17ef14dd38dd6ffd34a
Diego Baes <diegobaesv@gmail.com>
2024-05-27 21:56:37 -0500

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+
+export const routes: Routes = [
+    {
+        path: 'principal', component: MainLayoutComponent
+    }
+];
