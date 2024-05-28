ef45f09f87816bacd97fbdce5daafbede9e596cf
Diego Baes <diegobaesv@gmail.com>
2024-05-27 22:18:20 -0500

    primeras rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { AlumnosListarComponent } from './modules/alumnos/alumnos-listar/alumnos-listar.component';
+import { DocentesListarComponent } from './modules/docentes/docentes-listar/docentes-listar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: 'principal', component: MainLayoutComponent
-    }
+    { 
+        path: 'principal', component: MainLayoutComponent, children: [
+            { path: '', component: HomeComponent },
+            { path: 'alumnos', component: AlumnosListarComponent },
+            { path: 'docentes', component: DocentesListarComponent }
+        ]
+    },
+    
 ];
--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
