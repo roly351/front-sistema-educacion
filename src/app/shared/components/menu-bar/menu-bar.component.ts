56b8462b7d5a9f386bd7e2c31ae9177d11fd0334
Diego Baes <diegobaesv@gmail.com>
2024-06-03 22:18:46 -0500

    nuevos cambios al menu.bar

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,69 +1,90 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
 
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [ MenubarModule, ButtonModule, MenuModule, ToastModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
 export class MenuBarComponent implements OnInit {
 
-  items: MenuItem[] | undefined;
+  itemsMenuBar: MenuItem[] | undefined;
+
+  itemsMenuUsuario: MenuItem[] | undefined;
 
   ngOnInit() {
-    this.items = [
-        {
-            label: 'Personas',
-            icon: 'pi pi-users',
-            items: [
-              {
-                label: 'Alumnos',
-                icon: 'pi pi-graduation-cap'
-              },
-              {
-                label: 'Docentes',
-                icon: 'pi pi-id-card'
-              }
-            ]
-        },
-        {
-            label: 'Institucion',
-            icon: 'pi pi-briefcase',
-            items: [
-              {
-                label: 'Sedes',
-                icon: 'pi pi-building'
-              },
-              {
-                label: 'Cursos',
-                icon: 'pi pi-book'
-              },
-              {
-                label: 'Clases',
-                icon: 'pi pi-bookmark'
-              }
-            ]
-        },
-        {
-            label: 'Administrador',
-            icon: 'pi pi-cog',
-            items: [
-                {
-                    label: 'Usuarios',
-                    icon: 'pi pi-user'
-                },
-                {
-                    label: 'Bitacora',
-                    icon: 'pi pi-clipboard'
-                }
-            ]
-        }
+    this.itemsMenuUsuario = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh'
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload'
+          }
+        ]
+      }
+    ];
+
+    this.itemsMenuBar = [
+      {
+        label: 'Personas',
+        icon: 'pi pi-users',
+        items: [
+          {
+            label: 'Alumnos',
+            icon: 'pi pi-graduation-cap'
+          },
+          {
+            label: 'Docentes',
+            icon: 'pi pi-id-card'
+          }
+        ]
+      },
+      {
+        label: 'Institucion',
+        icon: 'pi pi-briefcase',
+        items: [
+          {
+            label: 'Sedes',
+            icon: 'pi pi-building'
+          },
+          {
+            label: 'Cursos',
+            icon: 'pi pi-book'
+          },
+          {
+            label: 'Clases',
+            icon: 'pi pi-bookmark'
+          }
+        ]
+      },
+      {
+        label: 'Administrador',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Usuarios',
+            icon: 'pi pi-user'
+          },
+          {
+            label: 'Bitacora',
+            icon: 'pi pi-clipboard'
+          }
+        ]
+      }
     ]
-}
+  }
 
 
 }
