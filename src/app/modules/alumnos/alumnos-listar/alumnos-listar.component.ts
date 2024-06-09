dc0d001e80ba9f0742752fed99babbe1b97a981a
Diego Baes <diegobaesv@gmail.com>
2024-06-08 22:07:29 -0500

    componente almnos modal

--- src/app/modules/alumnos/alumnos-listar/alumnos-listar.component.ts
+++ src/app/modules/alumnos/alumnos-listar/alumnos-listar.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import { Component, ViewChild } from '@angular/core';
+import { Table, TableModule } from 'primeng/table';
 import { IAlumno } from '../../../core/models/IAlumno';
 import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { DialogService } from 'primeng/dynamicdialog';
+import { AlumnosModalComponent } from '../alumnos-modal/alumnos-modal.component';
+
 
 @Component({
   selector: 'app-alumnos-listar',
   standalone: true,
-  imports: [TableModule, ButtonModule],
+  imports: [TableModule, ButtonModule,IconFieldModule,InputIconModule, InputTextModule],
   templateUrl: './alumnos-listar.component.html',
-  styleUrl: './alumnos-listar.component.scss'
+  styleUrl: './alumnos-listar.component.scss',
+  providers: [DialogService]
 })
 export class AlumnosListarComponent {
+
+  constructor(
+    private dialogService: DialogService
+  ){
+
+  }
+
+  @ViewChild('tblAlumnosListar') tblAlumnosListar: Table | undefined;
+
 
   buttonStyle= {
     width: '2.3rem',
     height: '2.3rem',
     'margin-left': '0.5rem'
   }
-
-
 
 
   alumnos: IAlumno[] = [{
@@ -43,5 +57,46 @@ export class AlumnosListarComponent {
     direccion:'Santa Beatriz',
     fechaNacimiento: '19/12/2000',
     sexo:'M'
-  }] 
+  }];
+
+  applyFilterGlobal($event:any, stringVal:string) {
+    this.tblAlumnosListar?.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
+
+  onClickCrearAlumno(){
+    console.log('onClickCrearAlumno');
+    this.dialogService.open(AlumnosModalComponent,
+      {
+        header:'Crear un Alumno', 
+        width: '70vw',
+        height: '80vh'
+      }
+    );
+  }
+
+  onClickVerAlumno(alumno: IAlumno){
+    console.log('onClickVerAlumno',alumno);
+    this.dialogService.open(AlumnosModalComponent,
+      {
+        header:'Información de Alumno', 
+        width: '70vw',
+        height: '80vh'
+      }
+    );
+  }
+
+  onClickEditarAlumno(alumno: IAlumno){
+    console.log('onClickEditarAlumno',alumno);
+    this.dialogService.open(AlumnosModalComponent,
+      {
+        header:'Modificar al Alumno', 
+        width: '70vw',
+        height: '80vh'
+      }
+    );
+  }
+
+  
+
+
 }
---
+++ src/app/modules/alumnos/alumnos-modal/alumnos-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './alumnos-modal.component.html',
+  styleUrl: './alumnos-modal.component.scss'
+})
+export class AlumnosModalComponent {
+
+}
